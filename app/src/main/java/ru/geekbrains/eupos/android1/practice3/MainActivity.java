cbfd6143bbf623a62fbe470579328007489b5113
Eupos <eupos@mail.ru>
2022-01-30 18:09:26 +0300

    Установил начальную позицию, окончание второго задания.

---
+++ app/src/main/java/ru/geekbrains/eupos/android1/practice3/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.eupos.android1.practice3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
