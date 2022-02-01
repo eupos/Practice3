31ea5994963c9e48a9ff8208ba50766bceab0c5b
Eupos <eupos@mail.ru>
2022-02-01 19:09:08 +0300

    Создал переменные

--- app/src/main/java/ru/geekbrains/eupos/android1/practice3/MainActivity.java
+++ app/src/main/java/ru/geekbrains/eupos/android1/practice3/MainActivity.java
@@ -3,8 +3,36 @@ package ru.geekbrains.eupos.android1.practice3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultField, operationField;
+    private  EditText numberField;
+    private Button btnNull;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEigh;
+    private Button btnNine;
+    private Button btnAdd;
+    private Button btnSub;
+    private Button btnMulti;
+    private Button btnDiv;
+    private Button btnPercent;
+    private Button btnResult;
+    private Button btnComma;
+    private Button btnBspace;
+    private Button btnClear;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
