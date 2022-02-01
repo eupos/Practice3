80d53b170f8a9ef06f7e986ecda76e41aae260a8
Eupos <eupos@mail.ru>
2022-02-01 20:47:34 +0300

    Привязал переменные к кнопкам и исправил ошибки

--- app/src/main/java/ru/geekbrains/eupos/android1/practice3/MainActivity.java
+++ app/src/main/java/ru/geekbrains/eupos/android1/practice3/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnEigh;
     private Button btnNine;
     private Button btnAdd;
-    private Button btnSub;
+    private Button btnSubtrac;
     private Button btnMulti;
     private Button btnDiv;
     private Button btnPercent;
@@ -31,12 +31,34 @@ public class MainActivity extends AppCompatActivity {
     private Button btnBspace;
     private Button btnClear;
 
-    public MainActivity() {
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("Калькулятор");
+        // РїРѕР»СѓС‡Р°РµРј РІСЃРµ РїРѕР»СЏ РїРѕ id РёР· activity_main.xml
+        resultField =(TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
+        btnNull = (Button) findViewById(R.id.btnNull);
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnFour = (Button) findViewById(R.id.btnFour);
+        btnFive = (Button) findViewById(R.id.btnFive);
+        btnSix = (Button) findViewById(R.id.btnSix);
+        btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnEigh = (Button) findViewById(R.id.btnEigh);
+        btnNine = (Button) findViewById(R.id.btnNine);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnComma = (Button) findViewById(R.id.btnComma);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnMulti = (Button) findViewById(R.id.btnMulti);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnResult = (Button) findViewById(R.id.btnResult);
+        btnSubtrac= (Button) findViewById(R.id.btnSubtrac);
+        btnBspace = (Button) findViewById(R.id.btnBspace);
+        btnPercent = (Button) findViewById(R.id.btnPercent);
     }
 }
